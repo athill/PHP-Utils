ab1bbdaf7b621425a1d8e656a1d19dcc9edab682
Andy Hill <andy@andyhill.us>
2015-02-15 13:28:17 -0500

    index.php fixed js style assoc array syntax

--- demo/index.php
+++ demo/index.php
@@ -2,8 +2,8 @@
 require_once('setup.inc.php');
 $local = [
 	'layout'=>[
-		'leftsidebar': [['type'=>'content', 'content'=>'left side bar']],
-		'rightsidebar': [['type'=>'content', 'content'=>'right side bar']],
+		'leftsidebar'=> [['type'=>'content', 'content'=>'left side bar']],
+		'rightsidebar'=> [['type'=>'content', 'content'=>'right side bar']],
 	]
 ];
 
