20c1c78863d29b09ca8fdee98fed60408f49ffc7
Andy Hill <andy@andyhill.us>
2015-02-15 14:33:00 -0500

    using gulp and less

---
+++ demo/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	watch = require('gulp-watch');
+ 
+gulp.task('less', function () {
+console.log('less');
+  gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./css'));
+});
+
+var watcher = gulp.task('watch', function () {
+    gulp.watch('less/*.less', ['less']);
+});
+
+watcher.on('change', function (event) {
+   console.log('Event type: ' + event.type); // added, changed, or deleted
+   console.log('Event path: ' + event.path); // The path of the modified file
+});
+
