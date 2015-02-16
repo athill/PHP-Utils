96035e49587453780cac0846b3e0d50787f0b8e9
Andy Hill <andy@andyhill.us>
2015-02-16 18:28:56 -0500

    sitetitle->sitename

--- src/Templates/DefaultTemplate.php
+++ src/Templates/DefaultTemplate.php
@@ -15,7 +15,7 @@ class DefaultTemplate extends \Athill\Utils\TemplateBase {
 	protected function heading() {
 		global $h, $site;
 		$h->oheader();
-		$h->h1($site['sitetitle']);
+		$h->h1($site['sitename']);
 		$h->cheader();
 	}
 
